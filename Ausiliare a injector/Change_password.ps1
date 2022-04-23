77b401a840e641ae39dd670f047bdaaf996d94f5
dottorvenom <89797997+dottorvenom@users.noreply.github.com>
2022-04-23 08:13:33 +0200

    old files
    ok

---
+++ Ausiliare a injector/Change_password.ps1
@@ -0,0 +1,5 @@
+
+
+
+Set-ADAccountPassword -Identity $args[0] -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $args[1] -Force)
+#Set-ADAccountPassword -Identity "Administrator" -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Bracciano2021" -Force)
