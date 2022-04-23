9c90a26113343874dc271ee6b1b6b7847d79c859
dottorvenom <89797997+dottorvenom@users.noreply.github.com>
2022-04-23 08:23:07 +0200

    upgrade
    ok

--- Change_password.ps1
+++ Change_password.ps1
@@ -2,4 +2,4 @@
 
 
 Set-ADAccountPassword -Identity $args[0] -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $args[1] -Force)
-#Set-ADAccountPassword -Identity "Administrator" -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Bracciano2021" -Force)
+#Set-ADAccountPassword -Identity "Administrator" -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Password@@!" -Force)
